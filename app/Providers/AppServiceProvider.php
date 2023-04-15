1a110304d1dd678a72a8101c8bbab291bd48bb05
Your Name <you@example.com>
2023-04-15 16:04:10 +0900

    https対応

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -25,5 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(App::environment(['production'])){
+            URL::forceScheme('https');
+        }
     }
 }
